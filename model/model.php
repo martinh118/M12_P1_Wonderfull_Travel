e805072bbe8e4616f22149a07f8e3876ab3d60cc
Marc Peral <m.peral@sapalomera.cat>
2023-10-30 17:06:18 +0100

    Clase Reserves Afegida la clase de reserves amb una funcio que les llegeix i relacionada amb les ofertes. Tambe s'ha afegir el camp id a la clase oferta, ja que aixi la podem relacionar amb la reserva.

--- model/model.php
+++ model/model.php
@@ -1,16 +1,18 @@
 <?php
 
 
-class oferta
+class Oferta
 {
+    private $id;            // id de l'oferta
     private $continent;     // nom del continent
     private $pais;          // nom del pais (en el nostre cas es el pais)
     private $preu;          // preu de l'oferta
     private $pathImatges;   // ruta de les imatges de l'oferta en el sistema de fitxers del servidor
     private $durada_dies;   // durada de dies de l'oferta
 
-    public function __construct($continent, $pais, $preu, $pathImatges, $durada_dies)
+    public function __construct($id, $continent, $pais, $preu, $pathImatges, $durada_dies)
     {
+        $this->id = $id;
         $this->continent = $continent;
         $this->pais = $pais;
         $this->preu = $preu;
@@ -42,8 +44,55 @@ class oferta
     {
         return $this->durada_dies;
     }
+
+    public function getId()
+    {
+        return $this->id;
+    }
 }
 
+class reserva
+{
+    private $oferta;    // oferta a la que es fa la reserva. 
+    private $nom;
+    private $telefon;
+    private $quantitat_persones;
+    private $descompte;
+
+    public function __construct(Oferta $oferta, string $nom, $telefon, $quantitat_persones, $descompte)
+    {
+        $this->oferta = $oferta;
+        $this->nom = $nom;
+        $this->telefon = $telefon;
+        $this->quantitat_persones = $quantitat_persones;
+        $this->descompte = $descompte;
+    }
+
+    public function getOferta()
+    {
+        return $this->oferta;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function getTelefon()
+    {
+        return $this->telefon;
+    }
+
+    public function getQuantitatPersones()
+    {
+        return $this->quantitat_persones;
+    }
+
+    public function getDescompte()
+    {
+        return $this->descompte;
+    }
+}
 
 function conect()
 {
@@ -158,7 +207,7 @@ function obtenerObjetoOfertas(): array
 {
     $connect = conect();
     try {
-        $sql = 'SELECT c.nom AS "Continent", p.nom AS "Pais", o.preu AS "Preu", o.imatges AS "Ruta Imatges", o.durada_dies AS "Durada"
+        $sql = 'SELECT o.id, c.nom AS "Continent", p.nom AS "Pais", o.preu AS "Preu", o.imatges AS "Ruta Imatges", o.durada_dies AS "Durada"
                 FROM wonderfull_travel.pais p
                 RIGHT JOIN wonderfull_travel.continent c ON p.continent_id  = c.id
                 INNER JOIN wonderfull_travel.ofertes o ON p.id = o.pais_id;';
@@ -166,7 +215,8 @@ function obtenerObjetoOfertas(): array
         $statement->execute();
 
         while ($fila = $statement->fetch()) {
-            $ofertas[] = new oferta($fila['Continent'], $fila['Pais'], $fila['Preu'], $fila['Ruta Imatges'], $fila['Durada']);
+            $ofertaId = $fila['id'];
+            $ofertas[$ofertaId] = new Oferta($ofertaId, $fila['Continent'], $fila['Pais'], $fila['Preu'], $fila['Ruta Imatges'], $fila['Durada']);
         }
         return $ofertas;
         // $statement->fetchAll();
@@ -177,6 +227,37 @@ function obtenerObjetoOfertas(): array
         // }
         // return $ofertas;
     } catch (PDOEXception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+function obtenerObjetoReservas(): array
+{
+    $connect = conect();
+    try {
+        // llegim les reserves, i en cas de tenir-ne, llegirem les ofertes i les asociarem des-del codi ja que si ho fem des de la consulta
+        // creariem una consulta massa gran i dificil de llegir degut a les relacions amb els paisos i la forma de mostrar les dades
+        $sql = 'SELECT r.client_nom AS "Nom", r.client_telefon AS "Telefon", r.quantitat_persones AS "Persones", r.descompte AS "Descompte", r.oferta_id AS "Oferta"
+                FROM wonderfull_travel.reserva r;';
+        $statement = $connect->prepare($sql);
+        $statement->execute();
+
+        // en cas de no tenir reserves retornem un array buid
+        if ($statement->rowCount() < 1) {
+            return array();
+        }
+
+        // llegim les ofertes (ja que les reserves son de ofertes)
+        $ofertes = obtenerObjetoOfertas();
+
+        // llegim els resultats de les reserves i creem un array de reserves
+        while ($fila = $statement->fetch()) {
+            $oferta = $ofertes[$fila['Oferta']];
+            $reservas[] = new reserva($oferta, $fila['Nom'], $fila['Telefon'], $fila['Persones'], $fila['Descompte']);
+        }
+
+        return $reservas;
+    } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
 }
