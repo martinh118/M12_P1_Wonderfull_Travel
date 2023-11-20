815999c49219dbc2edbbdc2132cf747345a1404b
Marc Peral <m.peral@sapalomera.cat>
2023-11-20 21:20:32 +0100

    Canviats els noms dels paisos a Castellá.

--- SQL/Inserir_Continents_i_Paisos.sql
+++ SQL/Inserir_Continents_i_Paisos.sql
@@ -6,7 +6,6 @@ VALUES ('Africa'),
     ('Nord América'),
     ('Oceania'),
     ('Sud América');
-    
 -- Obtener los IDs de los continentes recién insertados
 SET @idAfrica = LAST_INSERT_ID();
 SET @idAsia = @idAfrica + 1;
@@ -14,7 +13,6 @@ SET @idEuropa = @idAsia + 1;
 SET @idNordAmerica = @idEuropa + 1;
 SET @idOceania = @idNordAmerica + 1;
 SET @idSudAmerica = @idOceania + 1;
-
 -- Insertar algunos países asociados a los continentes
 INSERT INTO wonderfull_travel.pais (continent_id, nom)
 VALUES (@idAfrica, "Kenia"),
@@ -23,27 +21,27 @@ VALUES (@idAfrica, "Kenia"),
     (@idAfrica, "Botswana"),
     (@idAfrica, "Burkina Faso"),
     (@idAsia, "China"),
-    (@idAsia, "India"),
     (@idAsia, "Rusia"),
-    (@idAsia, "Pakistan"),
+    (@idAsia, "India"),
     (@idAsia, "Bangladesh"),
-    (@idEuropa, "Spain"),
-    (@idEuropa, "Holand"),
-    (@idEuropa, "Italy"),
-    (@idEuropa, "Croatia"),
-    (@idEuropa, "Cyprus"),
-    (@idNordAmerica, "United States"),
+    (@idAsia, "Pakistan"),
+    (@idEuropa, "España"),
+    (@idEuropa, "Holanda"),
+    (@idEuropa, "Italia"),
+    (@idEuropa, "Croacia"),
+    (@idEuropa, "Chipre"),
+    (@idNordAmerica, "Canada"),
+    (@idNordAmerica, "Estados Unidos"),
     (@idNordAmerica, "Mexico"),
     (@idNordAmerica, "Barbados"),
     (@idNordAmerica, "Belize"),
-    (@idNordAmerica, "Canada"),
     (@idOceania, "Australia"),
     (@idOceania, "Fiji"),
-    (@idOceania, "New Zealand"),
+    (@idOceania, "Nueva Zelanda"),
     (@idOceania, "Marshall Islands"),
     (@idOceania, "Micronesia"),
+    (@idSudAmerica, "Brazil"),
     (@idSudAmerica, "Argentina"),
     (@idSudAmerica, "Bolivia"),
-    (@idSudAmerica, "Brazil"),
-    (@idSudAmerica, "Peru"),
+    (@idSudAmerica, "Perú"),
     (@idSudAmerica, "Colombia");
--- controlador/index.js
+++ controlador/index.js
@@ -18,55 +18,55 @@
  * 				A les 08:00 AM del 29 de febrer: Bon dia. Avui és un dia especial.
  */
 function rellotge(ara) {
-	const weekday = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
+    const weekday = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
 
-	let tiempo = AMPM(ara); 
-	let diaSemana = ara.getDay();
-	let fecha = ara.getDate() + ' / ' + arreglarMes(ara) + ' / ' + ara.getFullYear();
-	
+    let tiempo = AMPM(ara);
+    let diaSemana = ara.getDay();
+    let fecha = ara.getDate() + ' / ' + arreglarMes(ara) + ' / ' + ara.getFullYear();
 
-	let texto =`${tiempo}<br>${weekday[diaSemana]}&nbsp${fecha}` ;
-	
-	return texto;
+
+    let texto = `${tiempo}<br>${weekday[diaSemana]}&nbsp${fecha}`;
+
+    return texto;
 }
 
-function arreglarMes(tiemp){
-	let meses = [1,2,3,4,5,6,7,8,9,10,11,12];
+function arreglarMes(tiemp) {
+    let meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-	return meses[tiemp.getMonth()];
-
-}
-
-function AMPM(tiempo){
-	let horasAM = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-	let horasPM = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-	let hora = 0;
-	if(tiempo.getHours() >= 12 && tiempo.getHours() <= 23){
-		hora = horasPM.indexOf(tiempo.getHours());
-		hora = horasAM[hora];
-		return cero(hora) + ":" + cero(tiempo.getMinutes()) + ":" + cero(tiempo.getSeconds())+' PM';
-	}else return cero(tiempo.getHours()) + ":" +cero(tiempo.getMinutes()) + ":" + cero(tiempo.getSeconds())+' AM';
+    return meses[tiemp.getMonth()];
 
 }
 
-function cero(num){
-	if(num < 10){
-		return '0'+num;
-	}else return num;
+function AMPM(tiempo) {
+    let horasAM = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    let horasPM = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+    let hora = 0;
+    if (tiempo.getHours() >= 12 && tiempo.getHours() <= 23) {
+        hora = horasPM.indexOf(tiempo.getHours());
+        hora = horasAM[hora];
+        return cero(hora) + ":" + cero(tiempo.getMinutes()) + ":" + cero(tiempo.getSeconds()) + ' PM';
+    } else return cero(tiempo.getHours()) + ":" + cero(tiempo.getMinutes()) + ":" + cero(tiempo.getSeconds()) + ' AM';
+
 }
 
-function selecPais(){
+function cero(num) {
+    if (num < 10) {
+        return '0' + num;
+    } else return num;
+}
+
+function selecPais() {
 
     let continente = document.getElementById("continent").value;
     let paises = document.getElementById("pais");
     let texto;
 
-    switch(continente){
+    switch (continente) {
         case "Asia":
             texto = "<option>China</option><option>Rusia</option><option>India</option><option>Bangladesh</option><option>Pakistan</option>";
             break;
         case "Europa":
-            texto = "<option>España</option><option>Holanda</option><option>Italia</option><option>Croacia</option><option>Cyprus</option>";
+            texto = "<option>España</option><option>Holanda</option><option>Italia</option><option>Croacia</option><option>Chipre</option>";
             break;
         case "Africa":
             texto = "<option>Kenia</option><option>Nigeria</option><option>Sudán</option><option>Botswana</option><option>Burkina Faso</option>";
@@ -78,61 +78,61 @@ function selecPais(){
             texto = "<option>Brazil</option><option>Argentina</option><option>Perú</option><option>Bolivia</option><option>Colombia</option>";
             break;
         case "Oceanía":
-            texto = "<option>Australia</option><option>Fiji</option><option>Nueva Zelanda</option><option>Marshall Island</option><option>Micronesia</option>";
+            texto = "<option>Australia</option><option>Fiji</option><option>Nueva Zelanda</option><option>Marshall Islands</option><option>Micronesia</option>";
             break;
         default:
             texto = "<option>China</option><option>Rusia</option><option>India</option><option>Bangladesh</option><option>Pakistan</option>";
             break;
     }
 
-    paises.innerHTML= texto;
+    paises.innerHTML = texto;
 
 }
 
-function selecImagenPais(){
+function selecImagenPais() {
     let pais = document.getElementById("pais").value;
 
-    const paises={
-        China: '../source/optimizadas/asia/china/china_peq.webp',
-        Rusia: '../source/optimizadas/asia/rusia/rusia_peq.webp',
-        India: '../source/optimizadas/asia/india/india_peq.webp',
-        Bangladesh: '../source/optimizadas/asia/bangladesh/bangladesh_peq.webp',
-        Pakistan: '../source/optimizadas/asia/pakistan/pakistan_peq.webp',
+    const paises = {
+        China: './source/optimizadas/asia/china/china_peq.webp',
+        Rusia: './source/optimizadas/asia/rusia/rusia_peq.webp',
+        India: './source/optimizadas/asia/india/india_peq.webp',
+        Bangladesh: './source/optimizadas/asia/bangladesh/bangladesh_peq.webp',
+        Pakistan: './source/optimizadas/asia/pakistan/pakistan_peq.webp',
 
-        España: '../source/optimizadas/europa/spain/spain_peq.webp',
-        Holanda: '../source/optimizadas/europa/holand/holand_peq.webp',
-        Italia: '../source/optimizadas/europa/italia/italia_peq.webp',
-        Croacia: '../source/optimizadas/europa/croatia/croatia_peq.webp',
-        Cyprus: '../source/optimizadas/europa/cyprus/cyprus_peq.webp',
+        España: './source/optimizadas/europa/españa/españa_peq.webp',
+        Holanda: './source/optimizadas/europa/holanda/holanda_peq.webp',
+        Italia: './source/optimizadas/europa/italia/italia_peq.webp',
+        Croacia: './source/optimizadas/europa/croacia/croacia_peq.webp',
+        Chipre: './source/optimizadas/europa/chipre/chipre_peq.webp',
 
-        Kenia: '../source/optimizadas/africa/kenia/kenia_peq.webp',
-        Nigeria: '../source/optimizadas/africa/nigeria/nigeria_peq.webp',
-        'Sudán': '../source/optimizadas/africa/sudan/sudan_peq.webp',
-        Botswana: '../source/optimizadas/africa/botswana/botswana_peq.webp',
-        'Burkina Faso': '../source/optimizadas/africa/burkina_faso/burkina_faso_peq.webp',
+        Kenia: './source/optimizadas/africa/kenia/kenia_peq.webp',
+        Nigeria: './source/optimizadas/africa/nigeria/nigeria_peq.webp',
+        'Sudán': './source/optimizadas/africa/sudán/sudan_peq.webp',
+        Botswana: './source/optimizadas/africa/botswana/botswana_peq.webp',
+        'Burkina Faso': './source/optimizadas/africa/burkina_faso/burkina_faso_peq.webp',
 
-        Canada: '../source/optimizadas/america_norte/canada/canada_peq.webp',
-        'Estados Unidos': '../source/optimizadas/america_norte/estados_unidos/estados_unidos_peq.webp',
-        Mexico: '../source/optimizadas/america_norte/mexico/mexico_peq.webp',
-        Barbados: '../source/optimizadas/america_norte/barbados/barbados_peq.webp',
-        Belize: '../source/optimizadas/america_norte/belize/belize_peq.webp',
+        Canada: './source/optimizadas/america_norte/canada/canada_peq.webp',
+        'Estados Unidos': './source/optimizadas/america_norte/estados_unidos/estados_unidos_peq.webp',
+        Mexico: './source/optimizadas/america_norte/mexico/mexico_peq.webp',
+        Barbados: './source/optimizadas/america_norte/barbados/barbados_peq.webp',
+        Belize: './source/optimizadas/america_norte/belize/belize_peq.webp',
 
-        Brazil: '../source/optimizadas/america_sur/brazil/brazil_peq.webp',
-        Argentina: '../source/optimizadas/america_sur/argentina/argentina_peq.webp',
-        'Perú': '../source/optimizadas/america_sur/peru/peru_peq.webp',
-        Bolivia: '../source/optimizadas/america_sur/bolivia/bolivia_peq.webp',
-        Colombia: '../source/optimizadas/america_sur/colombia/colombia_peq.webp',
+        Brazil: './source/optimizadas/america_sur/brazil/brazil_peq.webp',
+        Argentina: './source/optimizadas/america_sur/argentina/argentina_peq.webp',
+        Bolivia: './source/optimizadas/america_sur/bolivia/bolivia_peq.webp',
+        'Perú': './source/optimizadas/america_sur/perú/perú_peq.webp',
+        Colombia: './source/optimizadas/america_sur/colombia/colombia_peq.webp',
 
 
-        Australia: '../source/optimizadas/oceania/australia/australia_peq.webp',
-        Fiji: '../source/optimizadas/oceania/fiji/fiji_peq.webp',
-        'Nueva Zelanda': '../source/optimizadas/oceania/nueva_zelanda/nueva_zelanda_peq.webp',
-        'Marshall Island': '../source/optimizadas/oceania/marshall_islands/marshall_islands_peq.webp',
-        Micronesia: '../source/optimizadas/oceania/micronesia/micronesia_peq.webp'
+        Australia: './source/optimizadas/oceania/australia/australia_peq.webp',
+        Fiji: './source/optimizadas/oceania/fiji/fiji_peq.webp',
+        'Nueva Zelanda': './source/optimizadas/oceania/nueva_zelanda/nueva_zelanda_peq.webp',
+        'Marshall Islands': './source/optimizadas/oceania/marshall_islands/marshall_islands_peq.webp',
+        Micronesia: './source/optimizadas/oceania/micronesia/micronesia_peq.webp'
     }
 
     document.getElementById("imagen").src = paises[pais] || "";
-    
+
 }
 
 
@@ -142,11 +142,12 @@ function selecImagenPais(){
 ************************************************/
 
 function clock() {
-	document.getElementById('data').innerHTML = rellotge(new Date());
+    document.getElementById('data').innerHTML = rellotge(new Date());
 }
 
 setInterval(clock, 1000);
-document.getElementById("continent").addEventListener("change", function(){
+document.getElementById("continent").addEventListener("change", function () {
     selecPais();
-    selecImagenPais();});
+    selecImagenPais();
+});
 document.getElementById("pais").addEventListener("change", selecImagenPais);
