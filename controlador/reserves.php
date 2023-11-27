98dad66fc0321995d6319eec6c413b25186fe39f
Marc Peral <m.peral@sapalomera.cat>
2023-11-27 20:16:15 +0100

    afegides comprovacions de les dades d'entrada

--- controlador/reserves.php
+++ controlador/reserves.php
@@ -8,38 +8,51 @@ class ControladorReserves
 
     public static function inserirReserva()
     {
-        // comprovem si hi ha error amb algun camp introduit        
+        // comprovem si hi ha error amb algun camp introduit
+
+        // comprovacio id oferta
         if (empty($_POST["oferta-id"]) || $_POST["oferta-id"] == "-1") {
             $alertMessage = "S'ha produit un error, torna-ho a intentar. Si l'error persisteix, contacta amb un administrador.";
         };
         $dadesReserva["oferta-id"] = $_POST["oferta-id"];
 
+        // comprovacio data inici
+        if (empty($_POST["data-viatge"]) || strtotime($_POST["data-viatge"]) === false) {
+            $alertMessage = "Has d'introduir una data d'inici vàlida.";
+        } else {
+            $dataIntroduida = strtotime($_POST["data-viatge"]);
+            $dataActual = strtotime(date('Y-m-d'));
 
-        if (empty($_POST["data-viatge"])) {
-            $alertMessage = "Has d'introduir una data d'inici.";
-        };
-        $dadesReserva["dataInici"] = $_POST["data-viatge"];
+            if ($dataIntroduida <= $dataActual) {
+                $alertMessage = "La data d'inici ha de ser una data futura.";
+            } else {
+                $dadesReserva["dataInici"] = date('Y-m-d', $dataIntroduida);
+            }
+        }
 
+        // comprovacio nom
         if (empty($_POST["nom-client"])) {
             $alertMessage = "Has d'introduir un nom.";
         };
         $dadesReserva["nom"] = $_POST["nom-client"];
 
-        if (empty($_POST["telefon-client"])) {
-            $alertMessage = "Has d'introduir un telefon.";
-        };
+        // comprovacio telefon
+        if (empty($_POST["telefon-client"]) || !preg_match('/^[0-9]{9}$/', $_POST["telefon-client"])) {
+            $alertMessage = "Has d'introduir un telèfon vàlid de 9 dígits.";
+        }
         $dadesReserva["telefon"] = $_POST["telefon-client"];
 
-        if (empty($_POST["persones-client"])) {
-            $alertMessage = "Has d'introduir una quantitat de persones.";
-        };
+        // comprovacio quantitat persones
+        if (
+            empty($_POST["persones-client"]) || !ctype_digit($_POST["persones-client"]) || $_POST["persones-client"] <= 0
+        ) {
+            $alertMessage = "Has d'introduir una quantitat de persones vàlida.";
+        }
         $dadesReserva["quantitat_persones"] = $_POST["persones-client"];
 
-        if (empty($_POST["descompte"])) {
-            $dadesReserva["descompte"] = false;
-        } else {
-            $dadesReserva["descompte"] = true;
-        };
+        // comprovacio descompte
+        $dadesReserva["descompte"] = !empty($_POST["descompte"]) ? true : false;
+
 
         // TODO: COMPROVAR SI LES DADES INTRODUIDES SON CORRECTES
 
